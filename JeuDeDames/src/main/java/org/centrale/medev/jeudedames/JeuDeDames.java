4e039c91c24c36104951f7bd3490eb74e05d2c9a
Omykronn <omykronn.player@gmail.com>
2024-12-05 11:41:12 +0100

    Initialisation

---
+++ JeuDeDames/src/main/java/org/centrale/medev/jeudedames/JeuDeDames.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.medev.jeudedames;
+
+/**
+ * Main script
+ * @author simon
+ */
+public class JeuDeDames {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
