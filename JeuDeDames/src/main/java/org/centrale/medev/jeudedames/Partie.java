02cb1d6118af8f25a37db604d0c2ee1b6056f8dd
grigm <manon.grignon@eleves.ec-nantes.fr>
2024-12-05 11:44:26 +0100

    Class Partie with getters and setters

---
+++ JeuDeDames/src/main/java/org/centrale/medev/jeudedames/Partie.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.medev.jeudedames;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author grigm
+ */
+public class Partie {
+    private ArrayList<Pion> listePions; 
+
+    public ArrayList<Pion> getListePions() {
+        return listePions;
+    }
+
+    public void setListePions(ArrayList<Pion> listePions) {
+        this.listePions = listePions;
+    }
+    
+}
